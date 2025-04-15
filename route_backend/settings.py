856c9ccc54bfa6aa6d9d1e46b33b08f71603990f
Toshiki <mt.toshiki64@gmail.com>
2025-04-15 16:58:51 +0900

    second commit

--- route_backend/settings.py
+++ route_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
